81de2163628c76986ce79fd4c06f35b9fb05372a
nithish063 <63809345+nithish063@users.noreply.github.com>
2025-06-02 23:15:52 +0530

    Delete test_app.py

--- test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_home_page():
-    tester = app.test_client()
-    response = tester.get('/')
-    assert response.status_code == 200
-    assert b'Hello Nitish' in response.data
